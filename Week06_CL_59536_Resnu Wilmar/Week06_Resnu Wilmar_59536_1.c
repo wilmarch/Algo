6abc4b498d975e513cce38fe984677ba4fca58c4
Resnu Wilmar <100142613+wilmarch@users.noreply.github.com>
2022-03-07 18:26:19 +0700

    Update Week06_Resnu Wilmar_59536_1.c

--- Week06_CL_59536_Resnu Wilmar/Week06_Resnu Wilmar_59536_1.c
+++ Week06_CL_59536_Resnu Wilmar/Week06_Resnu Wilmar_59536_1.c
@@ -1,68 +1,58 @@
 #include <stdio.h>
 #include <malloc.h>
 
-void swap(int *a, int*b)
+void swap(int *a, int *b)
 {
-   int temp;
-   temp = *a;
-   *a = *b;
-   *b = temp;
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int partition(int *bil, int l, int r)
 {
     int pivot = bil[r];
-    int i = l-1;
-
-    for (int j=l; j<=r-1; j++)
+    int i = l - 1;
+    int j;
+    for (j = l; j <= r - 1; j++)
     {
-        if (bil[j] <= pivot)
+        if (bil[j] >= pivot)
         {
             i++;
             swap(&bil[i], &bil[j]);
         }
     }
-
     swap(&bil[i + 1], &bil[r]);
-
-    return(i + 1);
+    return (i + 1);
 }
 
-void quicksort(int *bil, int l, int r)
+void quickSort(int *bil, int l, int r)
 {
     if (l < r)
     {
         int pi = partition(bil, l, r);
-
-        quicksort(bil, l, pi - 1);
-        quicksort(bil, pi + 1, r);
+        quickSort(bil, l, pi - 1);
+        quickSort(bil, pi + 1, r);
     }
 }
-
 int main(int argc, char const *argv[])
 {
-     int *bil;
-     int n;
-     int i;
+    int *bil, i, n;
 
-     printf("Jumlah bilangan : ");
-     scanf("&d", &n);
+    printf("Jumlah bilangan: ");
+    scanf("%d", &n);
+    bil = (int *)malloc(sizeof(int) * n);
+    for (i = 0; i < n; i++)
+    {
+        printf("Masukkan bilangan : ");
+        scanf("%d", &bil[i]);
+    }
+    quickSort(bil, 0, n - 1);
 
-     bil = (int *)malloc(sizeof(int)*n);
-
-     for(i = 0; i < n; i++)
-     {
-         scanf("%d", &bil[i]);
-     }
-
-     quicksort(bil, 0, n-1);
-
-     printf("Hasil pengurutan: ");
-     for(i = 0; i < n; i++)
-     {
-         printf("%d ", bil[i]);
-     }
-
-     return 0;
+    printf("Hasil pegurutan: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", bil[i]);
+    }
+    return 0;
 }
-
